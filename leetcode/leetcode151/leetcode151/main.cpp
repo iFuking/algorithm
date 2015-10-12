a7cd2ed8f5c5b24765227c87236f8e549c709d54
iFuking <ChenminWang.Dk@gmail.com>
2015-10-12 23:05:43 +0800

    finish leetcode string tag

--- leetcode/leetcode151/leetcode151/main.cpp
+++ leetcode/leetcode151/leetcode151/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     void reverseWords(string &s) {
-        
+		if (s.length() == 0) return;
+
+		vector<string> words; string word = "";
+		for (int i = 0; i < s.length(); ++i) {
+			if (s[i]==' ' && word.length()>0) { words.push_back(word); word = ""; }
+			else if (s[i] != ' ') word += s[i];
+		}
+		if (word != "") words.push_back(word);
+		
+		s = "";
+		if (words.size() == 0) return;
+		for (int i = words.size()-1; i > 0; --i) { s += words[i]; s += " "; }
+		s += words[0];
+		return;
     }
 };
 
 int main()
 {
-	Solution sol; string s = "";
+	Solution sol; string s = "    ";
 	sol.reverseWords(s);
+	cout << s << endl;
 	return 0;
 }
---
+++ leetcode/leetcode43/leetcode43/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+const int maxn = 1e6 + 10;
+int a[maxn], b[maxn], m[maxn];
+char m_str[maxn];
+
+class Solution {
+public:
+	void swap(char &ch1, char &ch2)
+	{
+		char ch = ch1; ch1 = ch2; ch2 = ch;
+		return;
+	}
+
+    string multiply(string num1, string num2) {
+		if (num1=="" || num2=="") return "";
+		int non_zero;
+		for (non_zero = 0; non_zero<num1.length() && num1[non_zero]=='0'; ++non_zero) ;
+		num1 = num1.substr(non_zero, num1.length());
+		for (non_zero = 0; non_zero<num2.length() && num2[non_zero]=='0'; ++non_zero) ;
+		num2 = num2.substr(non_zero, num2.length());
+		if (num1=="" || num2=="") return "0";
+
+		int len1 = num1.length(), len2 = num2.length();
+		for (int i = 0; i < len1/2; ++i) swap(num1[i], num1[len1-1-i]);
+		for (int i = 0; i < len2/2; ++i) swap(num2[i], num2[len2-1-i]);
+
+		memset(a, 0, sizeof(a)); memset(b, 0, sizeof(b)); memset(m, 0, sizeof(m));
+		memset(m_str, 0, sizeof(m_str));
+		for (int i = 0; i < len1; ++i) a[i] = num1[i]-'0';
+		for (int i = 0; i < len2; ++i) b[i] = num2[i]-'0';
+		for (int i = 0; i < len1; ++i) {
+			for (int j = 0; j < len2; ++j) {
+				m[i+j] += a[i]*b[j];
+				m[i+j+1] += m[i+j]/10;
+				m[i+j] %= 10;
+			}
+		}
+		int len = len1+len2;
+		while (!m[len-1]) --len;
+		for (int i = 0; i < len; ++i) m_str[i] = m[i]+'0';
+		m_str[len] = '\0';
+		for (int i = 0; i < len/2; ++i) swap(m_str[i], m_str[len-1-i]);
+		return m_str;
+    }
+};
+
+int main()
+{
+	Solution sol;
+	cout << sol.multiply("9", "9") << endl;
+	return 0;
+}

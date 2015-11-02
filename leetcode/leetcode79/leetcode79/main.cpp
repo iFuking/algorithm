a1fb2fb748d746fe44ee834aa4a2a2c454bef954
iFuking <ChenminWang.Dk@gmail.com>
2015-11-02 10:03:00 +0800

    leetcode79 word search (wrong answer)

--- leetcode/leetcode79/leetcode79/main.cpp
+++ leetcode/leetcode79/leetcode79/main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
 using namespace std;
 
+const int maxn = 1e3 + 10;
 int dir[][2] = {
-	{0, -1}, {1, 0}, {0, 1}, {-1, 0}
+	{-1, 0}, {0, 1}, {1, 0}, {0, -1}
 };
 vector<char> w;
+bool visit[maxn][maxn], is_find;
 
 class Solution {
 public:
 	void dfs(int row, int col, int index, vector<vector<char>>& board, string &word)
 	{
-		if (w.size() == word.length()) return;
+		if (w.size() == word.length()) { is_find = true; return; }
+		if (is_find || index==word.size()) return;
 		for (int i = 0; i < 4; ++i) {
 			int r = row+dir[i][0], c = col+dir[i][1];
-			if (r>=0 && r<board.size() && c>=0 && c<board[r].size()) {
-				w.push_back(word[index+1]);
+			if (r>=0 && r<board.size() && c>=0 && c<board[r].size() && 
+				!visit[r][c] && board[r][c]==word[index]) {
+				visit[r][c] = true; w.push_back(word[index]);
 				dfs(r, c, index+1, board, word);
-				w.pop_back();
+				visit[r][c] = false; w.pop_back();
 			}
 		}
 		return;
@@ -31,10 +36,18 @@ public:
 				if (board[i][j] == word[0]) pos.push_back(make_pair(i, j));
 			}
 		}
+		if (word.length()==1 && pos.size()>0) return true;
 
+		memset(visit, 0, sizeof(visit)); is_find = false;
 		w.push_back(word[0]);
-		for (int i = 0; i < pos.size(); ++i) dfs(pos[i].first, pos[i].second, 0, board, word);
-		return true;
+		for (int i = 0; i < pos.size(); ++i) {
+			int row = pos[i].first, col = pos[i].second;
+			visit[row][col] = true;
+			dfs(row, col, 1, board, word);
+			visit[row][col] = false;
+			if (is_find) return true;
+		}
+		return false;
     }
 };
 
@@ -45,11 +58,13 @@ int main()
 		{'S', 'F', 'C', 'S'}, 
 		{'A', 'D', 'E', 'E'}
 	};
-	vector<vector<char>> board(3);
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 4; ++j) board[i].push_back(b[i][j]);
+	char a[][1] = { {'a'} };
+	vector<vector<char>> board(1);
+	for (int i = 0; i < 1; ++i) {
+		for (int j = 0; j < 1; ++j) board[i].push_back(a[i][j]);
 	}
-	Solution sol; sol.exist(board, "ABCCED");
+	Solution sol;
+	cout << sol.exist(board, "ab") << endl;
 	return 0;
 }
 

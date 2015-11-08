58d251ea7e4cce5137e3a394ac8e4a3f8f55edab
iFuking <ChenminWang.Dk@gmail.com>
2015-11-08 12:09:08 +0800

    leetcode228, not finished

---
+++ leetcode/leetcode228/leetcode228/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+		vector<string> ans;
+		if (nums.size() == 0) return ans;
+
+		string summary;
+		for (int i = 0; i < nums.size(); ++i) {
+			
+		}
+    }
+};
+
+int main()
+{
+	return 0;
+}
